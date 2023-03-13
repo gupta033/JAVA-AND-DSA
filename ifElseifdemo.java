ad6c32ba39d5d4aa20779933fad8395e9708d7aa
gupta033 <117852154+gupta033@users.noreply.github.com>
2023-03-13 14:17:23 +0530

    Add files via upload

---
+++ ifElseifdemo.java
@@ -0,0 +1,16 @@
+
+public class ifElseifdemo {
+    public static void main(String[] args) {
+        int testscore = 97;
+        char grade;
+        if(testscore >= 90){
+            grade ='A';
+
+        }
+        
+            else {
+                grade = 'F';
+            }
+         System.out.println("gread=" + grade );
+        }     
+}
