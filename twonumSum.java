fc55bc30f3c24610a01a47e4061c2bc5a003f996
gupta033 <117852154+gupta033@users.noreply.github.com>
2023-04-07 10:40:07 +0530

    Add files via upload

---
+++ twonumSum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class twonumSum {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number is:");
+        a=sc.nextInt();
+        System.out.println("Enter b number is:");
+        b=sc.nextInt();
+        c=a+b;
+        System.out.println("Two number sum:"+c);
+    }
+}
